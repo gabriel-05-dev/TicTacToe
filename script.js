1a36b73ce293fee714d61bdf8614859d2744cb8e
gabriel-05-dev <g.luchita@protonmail.com>
2024-06-25 22:36:31 +0100

    added html file & added function to append to gameboard.

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function gameboard() {
-    const user1 = createUser('player1', 1, 'x');
-    const user2 = createUser('player2', 2, 'o')
-    const table = {
-        board : [],
+    let board = [];
+
+    function get_user_cur_move() {
+        const user1 = createUser('player1', 1, 'x');
+        const user2 = createUser('player2', 2, 'o')
+        let cur_move = 0;
+        move();
+
+        function move() {
+            if (cur_move % 2 == 0) {
+                let addPoint = prompt('What index would you like to play your move?');
+                board.splice(addPoint, user1.symbol, 0);
+                cur_move++;
+            } else if (cur_move % 2 != 0) {
+                let addPoint = prompt('What index would you like to play your move?');
+                board.splice(addPoint, user2.symbol, 0);
+                cur_move++;
+            }};
+        
+        console.log(board);
+
     };
-}
+
+
+
 
 function createUser(user, player_number, symbol) {
     const Player = user;
@@ -12,3 +30,4 @@ function createUser(user, player_number, symbol) {
     const player_symbol = symbol;
     return {Player, Player_number, player_symbol};
 };
+
